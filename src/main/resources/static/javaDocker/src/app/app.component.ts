b53256edfa7151d9e2c7a1445f61f8dce92de01e
Facundo Navarro <navarromedina.facundo@gmail.com>
2022-10-25 00:06:23 -0300

    Angular ag-grid functional

--- src/main/resources/static/javaDocker/src/app/app.component.ts
+++ src/main/resources/static/javaDocker/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { ColDef } from 'ag-grid-community';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'javaDocker';
+  rowData:any[]=[];
+
+  colDeft: ColDef[]=[
+    {field:'make'},
+    {field: 'model'},
+    {field:'price'}
+  ]
 }
